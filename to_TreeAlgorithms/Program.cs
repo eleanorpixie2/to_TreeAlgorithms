e000ca83e75a13d8b68b8b1396e11b60ea381af4
Tess Olson <37564977+eleanorpixie2@users.noreply.github.com>
2018-10-14 21:51:08 -0500

    Updated with user input

--- to_TreeAlgorithms/Program.cs
+++ to_TreeAlgorithms/Program.cs
@@ -7,22 +7,95 @@ namespace to_TreeAlgorithms
     class Program
     {
         static bool IsReady;
+        static bool cont = true;
         static void Main(string[] args)
         {
             //load the text from file
             LoadText();
 
-            //print out the tree
-            Tree.WriteOutlineFile();
+            //creates a list of nodes
+            Tree.AddAllNodesToList();
+            
+            //give the user choices
+            while(cont)
+            {
+                Menu();
+            }
+        }
 
-            //keep application open
-            Console.ReadLine();
+        static void Menu()
+        {
+            int choice = 0;
+            string sChoice;
+            //get the user's menu choice
+            while (true)
+            {
+                Console.WriteLine("\n1-Find Node By Name\n2-Print Out to File\n3-Exit");
+                sChoice = Console.ReadLine();
+                try
+                {
+                    choice = Convert.ToInt32(sChoice);
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Please enter a vaild number");
+                    continue;
+                }
+            }
+            //based on choice, call function
+            switch(choice)
+            {
+                case 1:
+                    GetNode();
+                    break;
+                case 2:
+                    GetPathWay();
+                    break;
+                case 3:
+                    cont = false;
+                    break;
+            }
+        }
+
+        //get the output Pathway
+        static void GetPathWay()
+        {
+            Console.WriteLine("Enter output file pathway: ");
+            string s = Console.ReadLine();
+            //print out the tree
+            Tree.WriteOutlineFile(s);
+        }
+
+        //get node(s) to display with their children node values
+        static void GetNode()
+        {
+            Console.WriteLine("Please enter what value you wish to find");
+            string s = Console.ReadLine();
+            Console.Write("\n");
+            //list of node values
+            List<string>nodes=Tree.GetNode(s);
+            if(nodes.Count>0)
+            {
+                for(int i =0;i<nodes.Count;i++)
+                {
+                    //tab the children over
+                    Console.Write(nodes[i] + "\t");
+                }
+            }
+            else
+            {
+                //if value does not exist
+                Console.WriteLine("Value not found");
+            }
         }
 
         static void LoadText()
         {
+            Console.WriteLine("Enter input file pathway: ");
+            string s = Console.ReadLine();
             //read in the data
-            string[] lines = File.ReadAllLines(@"C:\workspace\unknownTaxonomy.txt");
+            string[] lines = File.ReadAllLines(s);
 
             //amount of white space before first word
             int whiteSpace = 0;
--- to_TreeAlgorithms/Tree.cs
+++ to_TreeAlgorithms/Tree.cs
@@ -10,6 +10,9 @@ namespace to_TreeAlgorithms
         //creates a list of root nodes
         static List<Node> tree = new List<Node>();
 
+        //contains all node values
+        static List<Node> nodes = new List<Node>();
+
         //the number of roots in the list
         static int root = 0;
 
@@ -26,18 +29,18 @@ namespace to_TreeAlgorithms
         }
 
         //display the tree
-        public static void WriteOutlineFile()
+        public static void WriteOutlineFile(string path)
         {
             for (int i = 0; i < tree.Count; i++)
             {
-                DisplayNodes(tree[i]);
+                DisplayNodes(tree[i],path);
             }
         }
 
         //display each node in the tree
-        public static void DisplayNodes(Node n)
+        public static void DisplayNodes(Node n,string s)
         {
-            using (StreamWriter file = new StreamWriter(@"C:\workspace\TaxonomyTest.txt", true))
+            using (StreamWriter file = new StreamWriter(s, true))
             {
 
                 //add the correct number of tabs for white space
@@ -53,34 +56,133 @@ namespace to_TreeAlgorithms
             //display each right most child node
             if (n.right != null)
             {
-                DisplayNodes(n.right);
+                DisplayNodes(n.right,s);
             }
             //display each center child node
             if (n.center != null)
             {
-                DisplayNodes(n.center);
+                DisplayNodes(n.center,s);
             }
             //display each left child node
             if (n.left != null)
             {
-                DisplayNodes(n.left);
+                DisplayNodes(n.left,s);
             }
             //display each leftcenter child node
             if (n.leftCenter != null)
             {
-                DisplayNodes(n.leftCenter);
+                DisplayNodes(n.leftCenter,s);
             }
             //display each centerleft child node
             if (n.centerLeft != null)
             {
-                DisplayNodes(n.centerLeft);
+                DisplayNodes(n.centerLeft,s);
             }
             //display each centerright child node
             if (n.centerRight != null)
             {
-                DisplayNodes(n.centerRight);
+                DisplayNodes(n.centerRight,s);
             }
 
+        }
+
+        public static List<string> GetNode(string s)
+        {
+            //list of nodes that match the string value
+            List<Node> n = new List<Node>();
+            //list of string values for each node that meets the string value
+            List<string> values = new List<string>();
+            for(int i=0;i<nodes.Count;i++)
+            {
+                if(nodes[i].value.Contains(s))
+                {
+                    n.Add(nodes[i]);
+                }
+            }
+            if (n != null)
+            {
+                for (int i = 0; i < n.Count; i++)
+                {
+                    //add node
+                    values.Add(n[i].value);
+                    //display right child
+                    if (n[i].right != null)
+                    {
+                        values.Add("\n\t" + n[i].right.value);
+                    }
+                    //display each center child node
+                    if (n[i].center != null)
+                    {
+                        values.Add("\n\t" + n[i].center.value);
+                    }
+                    //display each left child node
+                    if (n[i].left != null)
+                    {
+                        values.Add("\n\t" + n[i].left.value);
+                    }
+                    //display each leftcenter child node
+                    if (n[i].leftCenter != null)
+                    {
+                        values.Add("\n\t" + n[i].leftCenter.value);
+                    }
+                    //display each centerleft child node
+                    if (n[i].centerLeft != null)
+                    {
+                        values.Add("\n\t" + n[i].centerLeft.value);
+                    }
+                    //display each centerright child node
+                    if (n[i].centerRight != null)
+                    {
+                        values.Add("\n\t" + n[i].centerRight.value);
+                    }
+                }
+            }
+            //return string list
+            return values;
+        }
+
+        //add each node to a list
+        public static void AddAllNodesToList()
+        {
+            for (int i = 0; i < tree.Count; i++)
+            {
+                GetNodeValues (tree[i]);
+            }
+        }
+
+        static void GetNodeValues(Node n)
+        {
+            nodes.Add(n);
+            //display each right most child node
+            if (n.right != null)
+            {
+                GetNodeValues(n.right);
+            }
+            //display each center child node
+            if (n.center != null)
+            {
+                GetNodeValues(n.center);
+            }
+            //display each left child node
+            if (n.left != null)
+            {
+                GetNodeValues(n.left);
+            }
+            //display each leftcenter child node
+            if (n.leftCenter != null)
+            {
+                GetNodeValues(n.leftCenter);
+            }
+            //display each centerleft child node
+            if (n.centerLeft != null)
+            {
+                GetNodeValues(n.centerLeft);
+            }
+            //display each centerright child node
+            if (n.centerRight != null)
+            {
+                GetNodeValues(n.centerRight);
+            }
         }
 
     }
